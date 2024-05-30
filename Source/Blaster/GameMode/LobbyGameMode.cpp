16b4a2ed956c4a966992659d0f66c1384f8d8802
KimSungHun <kimtiger1201@naver.com>
2024-05-31 04:35:04 +0900

    ~variable replication
    .

--- Source/Blaster/GameMode/LobbyGameMode.cpp
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -11,9 +11,9 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
 	if (NumberOfPlayers == 2) {
 		UWorld* World = GetWorld();
-		/*if (World) {
+		if (World) {
 			bUseSeamlessTravel = true;
 			World->ServerTravel(FString("/Game/Maps/BlasterMap?listen"));
-		}*/
+		}
 	}
 }
