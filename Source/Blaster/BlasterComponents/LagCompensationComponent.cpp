d382b6994d7cb122560f45721e01111d35d45a67
KimSungHun <kimtiger1201@naver.com>
2024-08-24 17:53:37 +0900

    192. Rewinding Time

--- Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -35,6 +35,58 @@ void ULagCompensationComponent::SaveFramePackage(FFramePackage& OutPackage)
 	}
 }
 
+void ULagCompensationComponent::ServerSideRewind(ABlasterCharacter* HitCharacter, const FVector_NetQuantize& TraceStart, const FVector_NetQuantize& HitLocation, float HitTime)
+{
+	bool bReturn =
+		HitCharacter == nullptr ||
+		HitCharacter->GetLagCompensation() == nullptr ||
+		HitCharacter->GetLagCompensation()->FrameHistory.GetHead() == nullptr ||
+		HitCharacter->GetLagCompensation()->FrameHistory.GetTail() == nullptr;
+
+	//Frame package that we check to verify a hit
+	FFramePackage FrameToCheck;
+	bool bShouldInterpolate = true;
+	// Frame history of the HitCharacter
+	const TDoubleLinkedList<FFramePackage>& History = HitCharacter->GetLagCompensation()->FrameHistory;
+	const float OldestHistoryTime = History.GetTail()->GetValue().Time;
+	const float NewestHistoryTime = History.GetHead()->GetValue().Time;
+	if (OldestHistoryTime > HitTime) {
+		//too far back - too laggy to do SSR
+		return;
+	}
+	if (OldestHistoryTime == HitTime) {
+		FrameToCheck = History.GetTail()->GetValue();
+		bShouldInterpolate = false;
+	}
+	if (NewestHistoryTime <= HitTime) {
+		FrameToCheck = History.GetHead()->GetValue();
+		bShouldInterpolate = false;
+	}
+
+	TDoubleLinkedList<FFramePackage>::TDoubleLinkedListNode* Younger = History.GetHead();
+	TDoubleLinkedList<FFramePackage>::TDoubleLinkedListNode* Older = Younger;
+	while (Older->GetValue().Time > HitTime) { // is older still younger than hitTime?
+		//March back until : OlderTime < HitTime < YoungerTime
+		if (Older->GetNextNode() == nullptr) return;
+		Older = Older->GetNextNode();
+		if (Older->GetValue().Time > HitTime) {
+			Younger = Older;
+		}
+	}
+	if (Older->GetValue().Time == HitTime) { // highly unlikely, but we found our frame to check.
+		FrameToCheck = Older->GetValue();
+		bShouldInterpolate = false;
+	}
+	if (bShouldInterpolate) {
+		// Interpolate between Younger and Older
+
+	}
+
+
+	if (bReturn) return;
+
+}
+
 void ULagCompensationComponent::ShowFramePackage(const FFramePackage& Package, const FColor Color)
 {
 	for (auto& BoxInfo : Package.HitBoxInfo) {
